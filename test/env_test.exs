668a63e4763176cdbde0176d8eb67426efedaa9b
ityonemo <isaac.yonemoto@gmail.com>
2019-12-03 09:53:20 -0800

    tests on both string and atom proplists

---
+++ test/env_test.exs
@@ -0,0 +1,16 @@
+defmodule SSHTest.EnvTest do
+  use ExUnit.Case, async: true
+
+  describe "when executing SSH.run" do
+    @describetag :one
+    test "we can send environment variables as an atom list" do
+      conn = SSH.connect!("localhost")
+      assert SSH.run!(conn, "echo $foo", env: [foo: "bar"]) =~ "bar"
+    end
+
+    test "we can send environment variables as string prop list" do
+      conn = SSH.connect!("localhost")
+      assert SSH.run!(conn, "echo $foo", env: [{"foo", "bar"}]) =~ "bar"
+    end
+  end
+end
