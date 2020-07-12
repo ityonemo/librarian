d91bd78fc7c37c6bf677a7111da1595cec1f10aa
ityonemo <isaac.yonemoto@gmail.com>
2020-07-12 01:08:27 -0700

    add in the raising requirement test

--- test/stream_ingress_test.exs
+++ test/stream_ingress_test.exs
@@ -2,8 +2,8 @@ defmodule SSHTest.StreamIngressTest do
   use ExUnit.Case, async: true
 
   @footxt "/tmp/foo.txt"
+  @tag :one
   test "streaming to stdin over the connection is possible" do
-
     File.rm_rf!(@footxt)
 
     ssh_stream = "localhost"
@@ -17,6 +17,21 @@ defmodule SSHTest.StreamIngressTest do
     |> String.trim
 
     assert "3" == output
+  end
+
+  @tag :two
+  test "streaming into a ssh stream as a collectible will throw." do
+    File.rm_rf!(@footxt)
+
+    ssh_stream = "localhost"
+    |> SSH.connect!
+    |> SSH.stream!("tee /this-is-not-a-valid-file")
+
+    assert_raise RuntimeError, "", fn ->
+      ["foo"]
+      |> Enum.into(ssh_stream)
+      |> Enum.to_list
+    end
   end
 
 end
