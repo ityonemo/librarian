9e5090435c32f540795b1ae6c50386e5119ee64c
ityonemo <isaac.yonemoto@gmail.com>
2020-09-09 21:04:43 -0700

    verup

--- README.md
+++ README.md
@@ -83,7 +83,7 @@ in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:librarian, "~> 0.1.12"}
+    {:librarian, "~> 0.2.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Librarian.MixProject do
   def project do
     [
       app: :librarian,
-      version: "0.1.12",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
